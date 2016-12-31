8c71cf736b8b5063490a25c681d823ec574b50eb
Jeremy <jeremygrondahl@gmail.com>
2016-12-31 11:51:06 -0600

    Added side modal for artist bio. Changed color scheme of project.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,35 @@
 
 $(function() {
+  $('#about').hide();
   var animationName = "animated rubberBand";
   var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
   $('#title').on('click', function() {
     $(this).addClass(animationName).one(animationEnd, function(){
       $(this).removeClass(animationName);
-      $('.whiteSpace').addClass('visible');
+      $('body').addClass('visible');
+      $('#about').fadeIn(function(){
+        $(this).addClass('animated shake');
+      });
     });
   });
 });
 
+/*************
+TWITTER WIDGET
+**************/
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
